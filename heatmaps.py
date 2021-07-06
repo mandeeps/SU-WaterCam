6ffb984a8711299750dc9e72a79d21bdb76afa36
Manu <mandeeps@users.noreply.github.com>
2021-07-06 15:02:15 -0400

    display heatmaps of frames taken or average rate of change

--- heatmaps.py
+++ heatmaps.py
@@ -1,33 +1,50 @@
 #!/usr/bin/env python3
 # passed deriv csv, display heatmap of rate-of-change values
-import sys
+# passed data csv, display frames as heatmaps
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-def main(args):
-    print(args[1])
-    data = pd.read_csv('%s' % args[1])
-
-    rates = data.loc[:, data.columns.str.contains('Rate')]
-    #print(rates)
-    avgs = []
-    for column in rates:
-    # get average of change rate for each pixel
-        avgs.append(rates[column].mean())
-
-    #print(avgs)
+def main(args):    
+    df = pd.read_csv('%s' % args[1])
     
-    # save each pixel avg in 32x24 frame
+    if args[1].__contains__('deriv'):
+        rates = df.loc[:, df.columns.str.contains('Rate')]
+
+        avgs = []
+        for column in rates:
+        # get average of change rate for each pixel
+            avgs.append(rates[column].mean())
+        
+        colorMap = 'coolwarm_r'
+        array = avgs
+        show(array, colorMap)
+    
+    if args[1].__contains__('data'):
+        colorMap = 'coolwarm'
+        
+        # process each frame within the dataframe
+        for i in range(len(df)):
+            temps = df.values[i]
+            
+            array = []
+
+            for i in range(1, temps.size): # strip datetime values out 
+                array.append(temps[i])
+
+            show(array, colorMap)
+
+
+def show(array, colorMap):
+    # display frames, 32x24 res
     frame = []
     for row in range(24):
         line = []
         for pixel in range(32):
-            temp = avgs[row * 32 + pixel]
-            line.append(temp)
+            value = array[row * 32 + pixel]
+            line.append(value)
         frame.append(line)
-
-    sns.heatmap(frame, cmap='coolwarm', annot=True)
+    sns.heatmap(frame, cmap=colorMap, annot=True)
     plt.show()
 
 if __name__ == '__main__':
--- rateOfChange.py
+++ rateOfChange.py
@@ -13,8 +13,8 @@ import atexit
 i2c = busio.I2C(board.SCL, board.SDA, frequency=800000)
 mlx = adafruit_mlx90640.MLX90640(i2c)
 mlx.refresh_rate = adafruit_mlx90640.RefreshRate.REFRESH_2_HZ
-interval = 60 # time delay between each reading
-limit = 30 # number of frames to take
+interval = 4 # time delay between each reading
+limit = 10 # number of frames to take
 running = True
 df = pd.DataFrame()
 
@@ -32,16 +32,17 @@ def close():
     deriv = deriv.round(2)
     
     # local timezone
-    dataFile = 'data-%s.csv' % datetime.now().strftime('%Y%m%d-%H%M')
-    derivFile = 'deriv-%s.csv' % datetime.now().strftime('%Y%m%d-%H%M')
-    print('Writing data to %s and %s' % (dataFile, derivFile))
+    timeValue = datetime.now().strftime('%Y%m%d-%H%M')
+    dataFile = 'data-%s.csv' % timeValue 
+    derivFile = 'deriv-%s.csv' % timeValue
+    print('Writing data to %s, and %s' % (dataFile, derivFile))
 
-    # export DataFrames in csv format
+    # export DataFrames/list in csv format
     df.to_csv(dataFile)
     deriv.to_csv(derivFile)
 
-#for i in range(limit):
-while running:
+for i in range(limit):
+#while running:
     frame = pd.Series([], name = pd.to_datetime('now').replace(microsecond=0)) # UST timezone
     try:
         mlx.getFrame(frame)
