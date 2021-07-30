dbebc68a8db21aa883af240bab829047ab51c2ca
Manu <mandeeps@users.noreply.github.com>
2021-07-30 15:29:58 -0400

    further cleanup of temperature script

--- tools/temperature.py
+++ tools/temperature.py
@@ -2,23 +2,23 @@
 # get temp & humidity reading from Adafruit AHT20
 from time import sleep
 from datetime import datetime
-import csv
+from csv import DictWriter
 
 import board
 import adafruit_ahtx0
 
 SENSOR = adafruit_ahtx0.AHTx0(board.I2C())
+FILE = '/home/pi/HotWaterCam/tools/temps.csv'
 
 RUN = True
 while RUN:
-    row = {'Time':datetime.now().strftime('%Y%m%d-%H%M%S'),
-       'Temp': '%0.1f C' % SENSOR.temperature, 'Humidity': '%0.1f %%' %
-       SENSOR.relative_humidity}
-    print(row)
+    ROW = {'Time':datetime.now().strftime('%Y%m%d-%H%M%S'),
+        'Temp': '%0.1f C' % SENSOR.temperature, 'Humidity': '%0.1f %%' %
+        SENSOR.relative_humidity}
+    print(ROW)
 
-    with open('temperatures.csv', 'a', newline='') as out:
-        writer = csv.DictWriter(out, row.keys())
-        writer.writeheader()
-        writer.writerow(row)
+    with open(FILE, 'a+', newline='') as out:
+        DictWriter(out, ROW.keys()).writeheader()
+        DictWriter(out, ROW.keys()).writerow(ROW)
 
     sleep(60)
