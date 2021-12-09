134ed7863d0ce2e4c9bfcff2246a714cc34fec1c
Manu <mandeeps@users.noreply.github.com>
2021-12-09 08:26:29 -0500

    Add example script for MPU-6050 accelerometer

---
+++ tools/accel.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# Adafruit MPU-6050 example
+
+import time
+import board
+import adafruit_mpu6050
+
+i2c = board.I2C()  # uses board.SCL and board.SDA
+mpu = adafruit_mpu6050.MPU6050(i2c)
+
+while True:
+    print("Acceleration: X:%.2f, Y: %.2f, Z: %.2f m/s^2"%(mpu.acceleration))
+    print("Gyro X:%.2f, Y: %.2f, Z: %.2f degrees/s"%(mpu.gyro))
+    print("Temperature: %.2f C"%mpu.temperature)
+    print("")
+    time.sleep(0.33)
