f419a047c17ff6bda84bb28ea342a80805fd2df8
Manu <mandeeps@users.noreply.github.com>
2023-08-10 13:54:54 -0400

    tt_main.py compiles and runs with simulate.py, lepton function currently broken

--- tt_main.py
+++ tt_main.py
@@ -1,44 +1,18 @@
 # TT Python version
 
-from fractions import Fraction
 from ticktalkpython.SQ import SQify, STREAMify, GRAPHify
 from ticktalkpython.Clock import TTClock
 from ticktalkpython.Instructions import COPY_TTTIME, READ_TTCLOCK, VALUES_TO_TTTIME
 
-# two helper functions from https://gist.github.com/c060604/8a51f8999be12fc2be498e9ca56adc72
-def to_deg(value, loc):
-    """convert decimal coordinates into degrees, minutes and seconds tuple
-    Keyword arguments: value is float gps-value, loc is direction list ["S", "N"] or ["W", "E"]
-    return: tuple like (25, 13, 48.343 ,'N')
-    """
-    if value < 0:
-        loc_value = loc[0]
-    elif value > 0:
-        loc_value = loc[1]
-    else:
-        loc_value = ""
-    abs_value = abs(value)
-    deg =  int(abs_value)
-    t1 = (abs_value-deg)*60
-    minutes = int(t1)
-    sec = round((t1 - minutes)* 60, 5)
-    return (deg, minutes, sec, loc_value)
-
-def change_to_rational(number):
-    """convert a number to rational
-    Keyword arguments: number
-    return: tuple like (1, 2), (numerator, denominator)
-    """
-    f = Fraction(str(number))
-    return (f.numerator, f.denominator)
-
 @SQify
-def imu_values(trigger, DATA, image):
+def imu_values(trigger, image):
     import time
     from time import sleep
     import board
     import adafruit_bno055
 
+    fname = image[0]
+    DATA = '/home/pi/SU-WaterCam/data/data_log.txt'
     i2c = board.I2C()
     sensor = adafruit_bno055.BNO055_I2C(i2c)
     
@@ -49,7 +23,7 @@ def imu_values(trigger, DATA, image):
         "Gravity":sensor.gravity}
    
     # log IMU data to text file
-    imu_data = [f"\nFile: {image}\n",
+    imu_data = [f"\nFile: {fname}\n",
                 f"Time: {time.asctime(time.localtime(time.time()))}\n",
                 f"Accelerometer: {values['Accelerometer']}\n",
                 f"Gyro: {values['Gyro']}\n",
@@ -61,14 +35,16 @@ def imu_values(trigger, DATA, image):
     
     yaw, roll, pitch = values['Euler']
     
-    return values, roll, pitch, yaw
+    results = [roll, pitch, yaw]
+    return results
 
 @SQify
-def take_photo(trigger, filepath: str):
+def take_photo(trigger):
     from os import path
     from datetime import datetime
     from picamera2 import Picamera2
 
+    filepath = '/home/pi/SU-WaterCam/images/'
     picam2 = Picamera2()
     config = picam2.create_still_configuration(main={"format": "RGB888", "size": (2592,1944)})
     picam2.configure(config)
@@ -78,16 +54,17 @@ def take_photo(trigger, filepath: str):
     print(f'taking photo: {image}')
     picam2.start_and_capture_file(image, show_preview=False)
 
-    return image
+    return [image]
 
 @SQify
-def lepton_record(trigger, filepath: str):
+def lepton_record(trigger):
     from shutil import copy
     from os import path, mkdir, remove
     import subprocess # to call external apps
     from datetime import datetime
     import pytz
 
+    filepath = '/home/pi/SU-WaterCam/'
     # User configurable values
     TIMEZONE = pytz.timezone('US/Eastern') # Set correct timezone here
     # Local timezone
@@ -114,13 +91,45 @@ def lepton_record(trigger, filepath: str):
     # delete duplicated binaries
     remove(lepton)
     remove(capture)
+    return True
 
 @SQify
-def gps_data(trigger, DATA):
+def gps_data(trigger):
     import gpsd2
     import time
     import piexif
+    from fractions import Fraction
 
+    # two helper functions from https://gist.github.com/c060604/8a51f8999be12fc2be498e9ca56adc72
+    def to_deg(value, loc):
+        """convert decimal coordinates into degrees, minutes and seconds tuple
+        Keyword arguments: value is float gps-value, loc is direction list ["S", "N"] or ["W", "E"]
+        return: tuple like (25, 13, 48.343 ,'N')
+        """
+        if value < 0:
+            loc_value = loc[0]
+        elif value > 0:
+            loc_value = loc[1]
+        else:
+            loc_value = ""
+        abs_value = abs(value)
+        deg =  int(abs_value)
+        t1 = (abs_value-deg)*60
+        minutes = int(t1)
+        sec = round((t1 - minutes)* 60, 5)
+        return (deg, minutes, sec, loc_value)
+
+    def change_to_rational(number):
+        """convert a number to rational
+        Keyword arguments: number
+        return: tuple like (1, 2), (numerator, denominator)
+        """
+        f = Fraction(str(number))
+        return (f.numerator, f.denominator)
+
+
+
+    DATA = '/home/pi/SU-WaterCam/data/data_log.txt'
     gpsd2.connect()
         # get current gps info from gpsd
     gps_data = []
@@ -171,37 +180,45 @@ def gps_data(trigger, DATA):
                 }
 
         # Since we have GPS data, add to Exif
-        gps_exif = {"GPS": gps_ifd}
+        gps_exif = [{"GPS": gps_ifd}]
         print(gps_exif)
         return gps_exif
   
 
 @SQify
-def exif(trigger, image, gps_exif, roll, pitch, yaw):
+def exif(trigger, image, gps_exif, imu_data):
     import piexif
     import piexif.helper
     from libxmp import XMPFiles, consts
     print(image)
+    print(image[0])
+    fname = image[0]
     # load original exif data
-    exif_data = piexif.load(image)
+    exif_data = piexif.load(fname)
     # Add roll/pitch/yaw to UserComment tag if they exist
+    print(imu_data)
+    roll = imu_data[0]
+    pitch = imu_data[1]
+    yaw = imu_data[2]
+    print(f"Roll: {roll} Pitch: {pitch} Yaw: {yaw} \n")
     if roll:
         user_comment = piexif.helper.UserComment.dump(f"Roll {roll} Pitch {pitch} Yaw {yaw}")
         exif_data["Exif"][piexif.ExifIFD.UserComment] = user_comment
 
     # add gps tag to original exif data
     if gps_exif:
-        exif_data.update(gps_exif)
+        gpsdict = gps_exif[0]
+        exif_data.update(gpsdict)
 
     # Finish exif handling
     # convert to byte format for writing into file
     exif_bytes = piexif.dump(exif_data)
     # write to disk
-    piexif.insert(exif_bytes, image)
+    piexif.insert(exif_bytes, fname)
 
     # Write roll/pitch/yaw to XMP tags for Pix4D
     if roll:
-        xmpfile = XMPFiles(file_path=image, open_forupdate=True)
+        xmpfile = XMPFiles(file_path=fname, open_forupdate=True)
         xmp = xmpfile.get_xmp()
         xmp.set_property(consts.XMP_NS_DC, 'Roll', str(roll))
         xmp.set_property(consts.XMP_NS_DC, 'Pitch', str(pitch))
@@ -209,27 +226,21 @@ def exif(trigger, image, gps_exif, roll, pitch, yaw):
         xmpfile.put_xmp(xmp)
         xmpfile.close_file()
 
+    return True
+
 @GRAPHify
 def main(trigger):
-
-    # setup
-    DIRNAME = '/home/pi/SU-WaterCam/images/'
-    BASEDIR = '/home/pi/SU-WaterCam/'
-
-    # data record
-    DATA = '/home/pi/SU-WaterCam/data/data_log.txt'
-
     with TTClock.root() as root_clock:
         # take a new photo
-        image = take_photo(trigger, DIRNAME)
-        print(image)
+        image = take_photo(trigger)
+        
         # Call lepton and capture sequentially to get temperature and IR image
         # from Flir Lepton
-        lepton_record(trigger, BASEDIR)
+        
+        #runlepton = lepton_record(trigger)
 
         # get IMU data
-        imu_data, roll, pitch, yaw = imu_values(trigger, DATA, image)
-        print(imu_data)
-        gps_exif = gps_data(trigger, DATA)
+        imu_data = imu_values(trigger, image)
+        gps_exif = gps_data(trigger)
         
-        exif(trigger, image, gps_exif, roll, pitch, yaw)
+        runexif = exif(trigger, image, gps_exif, imu_data)
