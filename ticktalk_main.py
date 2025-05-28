336150892860de237c41782f8808e0ac7a6806e2
mandeeps <mandeeps@users.noreply.github.com>
2025-05-28 18:04:17 -0400

    rename photos script to clarify use with ticktalk

--- ticktalk_main.py
+++ ticktalk_main.py
@@ -1,4 +1,4 @@
-from take_nir_photos import flir, take_two_photos
+from tt_take_photos import flir, take_two_photos
 
 @STREAMify
 def get_time(trigger):
@@ -12,17 +12,26 @@ def get_time(trigger):
     return directory
 
 @SQify
-def coreg(dirname):
-    return None # return filename of coregistered image for use by SegFormer
+def coregistration(dirname, lepton_state, photo_state):
+    from coreg_multiple import coreg
+    print("\n running coreg \n")
+    filepath = coreg(dirname)
+    print(f"\n {filepath} \n")
+    return True
 
 @SQify
-def segformer(filepath): # operate on coregistered image file
+def segformer(filepath, coreg_state): # operate on coregistered image file
     import subprocess
+    segformer_python = "/home/pi/miniforge3/envs/5band/bin/python"
+    segformer_test = "/home/pi/git/segformer_5band/tools/test_no_label.py"
 
-    return None
+    segformer_coreg = "/home/pi/git/segformer_5band/tools/segment_coreg.py"
+
+    subprocess.Popen([segformer_python, segformer_test], cwd="/home/pi/git/segformer_5band")
+    return True
 
 @SQify
-def call_shutdown(flir_state, photo_state):
+def call_shutdown(state):
     import sys
 
     global sq_state
@@ -30,7 +39,7 @@ def call_shutdown(flir_state, photo_state):
     
     print(f"\n Iteration: {sq_state['count']} \n")
     
-    limit = 10
+    limit = 3
     if sq_state['count'] == limit:
         print("\n shutdown \n")
         sys.exit("shutdown") # schedule system shutdown and exit program
@@ -39,9 +48,12 @@ def call_shutdown(flir_state, photo_state):
 def ttmain(trigger):
     with TTClock.root() as root_clock:
 # main(trigger, "/home/pi") #x = test(trigger)
-        interval = 10_000_000
 
         dirname = get_time(trigger, TTClock=root_clock, TTPeriod=10_000_000, TTPhase=0, TTDataIntervalWidth=1_000_000)
-        x = take_two_photos(trigger, dirname, TTPeriod=10_000_000, TTPersistent=True)
+        photo = take_two_photos(trigger, dirname, TTPeriod=10_000_000, TTPersistent=True)
         lepton = flir(dirname, TTPeriod=10_000_000, TTPersistent=True)
-        y = call_shutdown(lepton, x)
+        
+        coreg_state = coregistration(dirname, lepton, photo)
+        segformer_state = segformer(dirname, coreg_state)
+
+        y = call_shutdown(segformer_state)
